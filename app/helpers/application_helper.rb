f153c95fb451f796c983588a376fbdd38f7e9f1b
Robert Rivas <robertrivas@gmail.com>
2012-02-20 21:14:55 -0800

    Modified syntax_highlight in application helper to reproduce correct code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,12 @@
 module ApplicationHelper
   def markdown(text)
-    # syntax_highlighter(Redcarpet::Markdown.new(Redcarpet::Render::HTML, *options).render(text)).html_safe
-    # syntax_highlighter(text).html_safe
     options = [filter_html: true, autolink: true, space_after_headers: true, fenced_code_blocks: true]
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, :hard_wrap => true).render(text).html_safe
-    syntax_highlighter(Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:hard_wrap => true), *options).render(text)).html_safe
+    syntax_highlighter(Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:hard_wrap => true), *options).render(text)).html_safe 
   end
 
   def syntax_highlighter(html)
     doc = Nokogiri::HTML(html)
     doc.search("//code[@class]").each{ |code| code.replace(Albino.colorize(code.text.rstrip, code[:class])) }
-    doc.css('body').inner_html.to_s
+    doc.css('pre:first').first.inner_html.to_s
   end
 end
