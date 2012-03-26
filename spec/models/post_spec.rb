756e773650bdb076dd4336f9fc0de051a3124866
Robert Rivas <robertrivas@gmail.com>
2012-03-25 20:43:30 -0700

    Added unit test for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do
+    before(:each) do
+      @post = Factory.build(:post)
+    end
+
+    it "should require a title" do
+      @post.title = ''
+      @post.should_not be_valid
+      @post.errors[:title].should_not be_empty
+    end
+
+    it "should require a description" do
+      @post.description = ''
+      @post.should_not be_valid
+      @post.errors[:description].should_not be_empty
+    end
+
+    it "should require content" do
+      @post.content = ''
+      @post.should_not be_valid
+      @post.errors[:content].should_not be_empty
+    end
+  end
+
 end
