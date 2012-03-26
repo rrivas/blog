8a106c0727e1e670578aab1567f4b82f13f159e3
Robert Rivas <robertrivas@gmail.com>
2012-03-25 20:42:56 -0700

    Removed some devise functions and changed the format of validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :recoverable, :registerable, :rememberable, :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :trackable, :validatable
 
   field :name
-  validates_presence_of :name, :email
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false}
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
