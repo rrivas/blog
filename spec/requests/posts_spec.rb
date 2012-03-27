73579a470abea36cdd48361e8f874c2a76265cc6
Robert Rivas <robertrivas@gmail.com>
2012-03-26 20:34:29 -0700

    Added Display post and new post to signed in user request test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,10 +2,24 @@ require 'spec_helper'
 
 describe "Posts" do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get posts_path
-      response.status.should be(200)
+    it "displays posts" do
+      Post.create!( :title => 'This is a new post', :description => 'This is a Description for the post', :content => 'Content' )
+      visit posts_path
+      page.should have_content('This is a new post')
+      page.should have_content('This is a Description for the post')
+    end
+
+    it "does not diesplay new posts to non signed in viewers" do
+      visit posts_path
+      page.should_not have_content("New Post")
+    end
+
+    it "should display new posts to signed in users" do
+      @user = Factory.create(:user)
+      login @user
+      visit posts_path
+      save_and_open_page
+      page.should have_content("New Post")
     end
   end
 end
