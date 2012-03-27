2aac4e1ae4e28da3e035c88773d37c243e611e6c
Robert Rivas <robertrivas@gmail.com>
2012-03-26 22:04:52 -0700

    Added more request tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,23 +3,65 @@ require 'spec_helper'
 describe "Posts" do
   describe "GET /posts" do
     it "displays posts" do
-      Post.create!( :title => 'This is a new post', :description => 'This is a Description for the post', :content => 'Content' )
+      @post = Factory.create(:post)
       visit posts_path
-      page.should have_content('This is a new post')
-      page.should have_content('This is a Description for the post')
+
+      page.should have_content(@post.title)
+      page.should have_content(@post.description)
     end
 
-    it "does not diesplay new posts to non signed in viewers" do
+    it "does not display new posts link to non signed in viewers" do
       visit posts_path
       page.should_not have_content("New Post")
     end
 
-    it "should display new posts to signed in users" do
+    it "displays new posts link to signed in users" do
       @user = Factory.create(:user)
       login @user
       visit posts_path
-      save_and_open_page
       page.should have_content("New Post")
+    end
+
+    it "does not display an edit link to non signed in users" do
+      @post = Factory.create(:post)
+      visit posts_path
+      page.should_not have_content("Edit")
+    end
+
+    it "displays an edit button to signed in users" do
+      @user = Factory.create(:user)
+      @post = Factory.create(:post)
+      login @user
+      visit posts_path
+      page.should have_content("Edit")
+    end
+
+
+    it "routes you to post edit when clicking on edit post to signed in users" do
+      @user = Factory.create(:user)
+      @post = Factory.create(:post)
+      login @user
+      visit posts_path
+      click_link('Edit')
+
+      page.current_path.should == edit_post_path(@post)
+    end
+
+    it "should take you to new post when clicking New Post link" do
+      @user = Factory.create(:user)
+      login @user
+      visit posts_path
+      click_link("New Post")
+
+      page.current_path.should == new_post_path
+    end
+
+    it "should take you to the post show page when you click a post link" do
+      @post = Factory.create(:post)
+      visit posts_path
+      click_link(@post.title)
+
+      page.current_path.should == post_path(@post)
     end
   end
 end
