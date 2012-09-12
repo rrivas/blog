dda89553141db7b59f4bd05c0a7e129c8669f9ee
Robert Rivas <robertrivas@gmail.com>
2012-09-11 19:17:40 -0700

    Added testing for home

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,13 +1,13 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe PageController do
+describe PageController do
 
-#   describe "GET 'home'" do
-#     it "returns http success" do
-#       get 'home'
-#       response.should be_success
-#     end
-#   end
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
 
 #   describe "GET 'projects'" do
 #     it "returns http success" do
@@ -30,4 +30,4 @@
 #     end
 #   end
 
-# end
+end
